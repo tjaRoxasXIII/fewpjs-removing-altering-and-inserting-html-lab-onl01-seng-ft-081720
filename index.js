dabe02c85b0812379bddf542e8ce9b944809d6be
Taylor <tjaroguetech@gmail.com>
2020-11-12 13:18:09 -0500

    commit tree 8cdd78afb7f899b9ed1f8d0177d3d2360b918df4

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Taylor is the champion"
+// let newHeader = document.querySelector("h1#victory");
+// newHeader.innerHTML = 'VICTORY!'
